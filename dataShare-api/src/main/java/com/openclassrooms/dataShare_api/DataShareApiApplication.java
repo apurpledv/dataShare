348dbfc272a7755435e90f85ccf754ddff90d37d
apurpledv <david.chicot.77@gmail.com>
2026-01-26 11:26:06 +0100

    initial commit

---
+++ dataShare-api/src/main/java/com/openclassrooms/dataShare_api/DataShareApiApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.dataShare_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataShareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataShareApiApplication.class, args);
+	}
+
+}
